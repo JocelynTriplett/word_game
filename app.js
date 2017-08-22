cd97db6babcdfc4176d6a6ac499ee0c2a73f87ba
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-22 15:22:32 -0400

    skeleton app runs

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
+const fs = require('fs');
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 const express = require('express');
 const mustache = require('mustache-express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
+
+
+// setup view engine
+app.engine('mustache', mustache());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+// from express-session documentation:
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.get('/',function(req,res){
+  res.render('index');
+});
+
+app.post('/', function(req, res){
+
+});
+
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000!')
+});
