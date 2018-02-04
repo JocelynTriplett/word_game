b2e6cffafb592b3da573a3eafe56b79f3bd614b5
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-02-03 20:56:05 -0500

    update listen to process.env.PORT || 5000

--- app.js
+++ app.js
@@ -187,6 +187,7 @@ app.post("/", function(req, res) {
   }
 });
 
-app.listen(5000, function() {
-  console.log("Example app listening on port 5000!");
-});
+app.listen(process.env.PORT || 5000);
+// app.listen(5000, function() {
+//   console.log("Example app listening on port 5000!");
+// });
